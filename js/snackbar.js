928e2f304f9f9a5f7f789c2251aaca0f8208c1f9
fengxi2233 <fengxi2233@gmail.com>
2023-02-15 19:53:20 +0800

    Site updated: 2023-02-15 19:53:18

--- js/snackbar.js
+++ js/snackbar.js
@@ -1,14 +1,8 @@
-if(window.location.href == 'http://localhost:4000/'||window.location.href == 'https://fengxi2233.love/'||window.location.href == 'https://fengxi2233.github.io/')
-Snackbar.show({
-       text: '欢迎来到枫汐的小站。',
-       width: '475px',
-       onActionClick: function(element) {
-           //Set opacity of element to 0 to close Snackbar
-           $(element).css('opacity', 0);
-           alert('Clicked Called!');
-       }
-    });
-
-    Snackbar.show({ actionText: '收到！', });
-
-    Snackbar.show({ actionTextColor: '#5fff9698', });      
+if(window.location.href == 'http://localhost:4000/'||window.location.href == 'https://www.fengxi2233.love/'||window.location.href == 'https://fengxi2233.love/'||window.location.href == 'https://fengxi2233.github.io/'){
+    Snackbar.show({ actionText: '收到',text: '欢迎来到丨枫汐丨的小站！',backgroundColor: '#55abe7ff',actionTextColor: '#55ff5f55',pos: 'top-center',duration: '5000' });
+  }
+onActionClick: function(element) {
+    //Set opacity of element to 0 to close Snackbar
+    $(element).css('opacity', 0);
+    alert('Clicked Called!');
+    }
