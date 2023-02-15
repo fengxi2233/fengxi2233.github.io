4ae639125537a15e7cd01b6e6277184c0d0ab798
fengxi2233 <fengxi2233@gmail.com>
2023-02-15 20:33:45 +0800

    Site updated: 2023-02-15 20:33:43

--- js/snackbar.js
+++ js/snackbar.js
@@ -1,3 +1,6 @@
+btf.snackbarShow('欢迎来到丨枫汐丨的小站！') 
+
+
 if(window.location.href == 'http://localhost:4000/'||window.location.href == 'https://www.fengxi2233.love/'||window.location.href == 'https://fengxi2233.love/'||window.location.href == 'https://fengxi2233.github.io/'){
     Snackbar.show({ actionText: '收到',text: '欢迎来到丨枫汐丨的小站！',backgroundColor: '#55abe7ff',actionTextColor: '#55ff5f55',pos: 'top-center',duration: '5000' });
   }
