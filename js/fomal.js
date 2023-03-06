e550c23741b6cb53b4a1b61581a8be3b21cf84e8
fengxi2233 <fengxi2233@gmail.com>
2023-03-06 11:24:03 +0800

    Site updated: 2023-03-06 11:24:03

--- js/fomal.js
+++ js/fomal.js
@@ -1321,11 +1321,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '👀跑哪里去了~';
+    document.title = '枫汐爱你哦';
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '🐖抓到你啦～';
+    document.title = '你看错啦';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
